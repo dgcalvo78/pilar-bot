8620c6160845c6725896aef749a256b34ce177cb
DaniG <dgcalvo78@gmail.com>
2021-03-19 17:21:45 +0100

    ayuda

--- index.js
+++ index.js
@@ -16,16 +16,23 @@ client.once('ready', () => {
 
 
 client.on("message", function(message) {
-    if (message.author.bot || !message.content.startsWith(prefix) && message.author.id !== '264477975628480514') return;
+
+    if (message.author.id === '264477975628480514') message.react('819963117534314506');
+
+    if (message.author.bot || !message.content.startsWith(prefix) ) return;
    
-    message.react('819963117534314506');
+    
 
     const commandBody = message.content.slice(prefix.length);
     const args = commandBody.split(' ');
     const command = args.shift().toLowerCase();
 
     if (command === "bracket") {
-        message.reply('mañana juega G2 y pasao tu puta madre <:jhinzhao:779837354659676269>')
+        message.reply('mañana juega G2 y pasao tu puta madre <:jhinzhao:779837354659676269>');
+    };
+
+    if (command === "ayuda") {
+        message.reply('r');
     };
 
     if(command === "pilar"){
